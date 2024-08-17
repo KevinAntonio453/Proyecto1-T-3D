64c6996b60db5632d638601b58e00782bb206dc1
KevinAntonio453 <101834494+KevinAntonio453@users.noreply.github.com>
2024-08-17 09:24:02 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PGrafica1
             var nativeWindowSettings = new NativeWindowSettings()
             {
                 ClientSize = new Vector2i(800, 600),
-                Title = "Proyecto 1: T(3D)",
+                Title = "Hola mundo Opengl",
             };
             using (Game game = new Game(GameWindowSettings.Default, nativeWindowSettings))
             {
